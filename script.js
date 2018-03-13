f2234bf6429b1276e3a884b880e689aedba2ea8d
MBTC2018 <mbtc2018@Amp17-16.local>
2018-03-13 14:15:19 -0400

    button edits

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-
-
-var myVariable = 20
-var myWord = "Snow"
-var number2 = 20
-
-//this is my function
-function saySomething(){
-
-        var result = myVariable + number2
-
-        console.log("This is Edwin typing" + result)
-
-        
+//this is my function for twtter
+function twitter(){
+    window.open("https://twitter.com/edwxn_lxn?lang=en")
 }
-
-function toggledoge(){
-    
-    var img = document.getElementById("dogpic")
-
-    if(showdog == false){
-        //hidden
-        img.style.visibility = "hidden"
-        showdog = true
-
-    }else if(showdog == true){
-        //revealed
-        img.style.visibility = "visible"
-        showdog = false
-
-    }
+//this is my function for instagram
+function instagram(){
+    window.open("https://www.instagram.com/edwin.lxn/")
 }
